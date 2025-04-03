c315292a2275aa5bfe7bfbcdd46cf7469c47dcd8
Mital Andipara <mital@logisticinfotech.co.in>
2025-04-03 18:55:50 +0530

    design related changes

--- src/app/(user)/friends/page.jsx
+++ src/app/(user)/friends/page.jsx
@@ -8,30 +8,24 @@ const FriendPage = () => {
     const router = useRouter();
 
     const friends = [
-        { id: 1, img: "/images/friend1.jpg", online: true },
-        { id: 2, img: "/images/friend2.jpg", online: false },
-        { id: 3, img: "/images/friend3.jpg", online: true },
-        { id: 4, img: "/images/friend4.jpg", online: true },
-        { id: 5, img: "/images/friend5.jpg", online: false },
-        { id: 6, img: "/images/friend6.jpg", online: true },
-        { id: 7, img: "/images/friend7.jpg", online: true },
-        { id: 8, img: "/images/friend8.jpg", online: true },
+        { id: 1, img: "/images/profile.jpg", online: true },
+        { id: 2, img: "/images/profile.jpg", online: false },
+        { id: 3, img: "/images/profile.jpg", online: true },
+        { id: 4, img: "/images/profile.jpg", online: true },
+        { id: 5, img: "/images/profile.jpg", online: false },
+        { id: 6, img: "/images/profile.jpg", online: true },
+        { id: 7, img: "/images/profile.jpg", online: true },
+        { id: 8, img: "/images/profile.jpg", online: true },
     ];
     return (
         <div className="flex flex-col h-full">
-            <div className="sticky top-0 bg-white w-full z-10 py-4 flex items-center">
+            <div className="sticky top-0 bg-white w-full z-10 p-4 flex items-center">
                 <button onClick={() => router.back()}>
                     <img src="/images/Back_Arrow_Icon.svg" alt="Back" />
                 </button>
                 <h1 className="mx-auto text-3xl font-bold">Amigos</h1>
             </div>
-            {/* <div>
-                <div className="password-main-div">
-                    <input type="text" name="search" placeholder="Buscar" className="relative common-input" />
-                    <img src="/images/search_icon.svg" className="password-icon-img pr-2" alt="search_icon" />
-                </div>
-            </div> */}
-            <div className="sticky top-[68px] bg-white z-10">
+            <div className="sticky top-[68px] px-4 bg-white z-10">
                 <div className="password-main-div">
                     <input type="text" name="search" placeholder="Buscar" className="relative common-input" />
                     <img src="/images/search_icon.svg" className="password-icon-img pr-2" alt="search_icon" />
@@ -51,13 +45,13 @@ const FriendPage = () => {
                     </div>
                 ))}
             </div> */}
-            <div className="pb-28 w-full my-5 mx-auto grid grid-cols-3 xs:grid-cols-4 sm:grid-cols-4 gap-3">
+            <div className="pb-28 px-4 w-full my-5 mx-auto grid grid-cols-3 xs:grid-cols-4 sm:grid-cols-4 gap-3">
                 {friends.map((friend) => (
                     <div key={friend.id} className="aspect-square relative">
                         <img
                             src={friend.img}
                             alt="Friend"
-                            className="w-full h-full rounded-full object-cover border-2 border-gray-400"
+                            className="w-full h-full rounded-full object-cover border border-white"
                         />
                         {friend.online && (
                             <span className="absolute top-2 right-2 w-4 h-4 bg-cyan border-2 rounded-full border-white"></span>
--- src/app/(user)/wardrobe/page.jsx
+++ src/app/(user)/wardrobe/page.jsx
@@ -1,10 +1,13 @@
 "use client";
 import BottomNavbar from "@/components/BottomNavbar";
+import TokenDetailsCard from "@/components/TokenDetails";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Wardrobe() {
     const router = useRouter();
+    const [isOpen, setIsOpen] = useState(false);
     const items = [
         { name: "Camiseta", type: "Ghost", image: "/images/White_Tee@3x.png" },
         { name: "Camiseta", type: "Demonio", image: "/images/White_Tee@3x.png" },
@@ -21,23 +24,27 @@ export default function Wardrobe() {
     return (
         <div>
             {/* Header */}
-            <div className="sticky top-0 bg-white w-full z-10 py-4 flex items-center">
+            <div className="app-header">
                 <button onClick={() => router.back()}>
                     <img src="/images/Back_Arrow_Icon.svg" alt="Back" />
                 </button>
-                <h1 className="mx-auto pr-4 text-3xl font-bold">Armario</h1>
+                <h1 className="app-title">Armario</h1>
+                <span className="w-[30px]" />
             </div>
 
             {/* Grid Items */}
             <div className="wardrobe-items">
                 {items.map((item, index) => (
-                    <div key={index} >
+                    <div key={index} onClick={() => setIsOpen(true)} >
                         <Image src={item.image} width={150} height={150} alt={item.type} className="wardrobe-item-card" />
                         <p className="wardabe-item-name">{item.name}</p>
                         <span className="font-bold">{item.type}</span>
                     </div>
                 ))}
             </div>
+            {isOpen && (
+                <TokenDetailsCard setIsOpen={setIsOpen} />
+            )}
             <div id="popup-modal" tabIndex={-1} className="popup-modal">
                 <div className="popup-content">
                     <h1 className="popup-title">¡Vas a tope!</h1>
@@ -56,7 +63,8 @@ export default function Wardrobe() {
                     <button className="go-to-wardrobe">Ir a mi armario</button>
                 </div>
             </div>
-            <BottomNavbar />
+            {!isOpen && (<BottomNavbar />)}
+
         </div>
     );
 }
